98eb2441fea5140d2229144824088c9345cd9307
Les1ie <iansmith@qq.com>
2024-06-12 14:31:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.les1ie.com": "https://registry-1.docker.io",
+  "quay.les1ie.com": "https://quay.io",
+  "gcr.les1ie.com": "https://gcr.io",
+  "k8s-gcr.les1ie.com": "https://k8s.gcr.io",
+  "k8s.les1ie.com": "https://registry.k8s.io",
+  "ghcr.les1ie.com": "https://ghcr.io",
+  "cloudsmith.les1ie.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
